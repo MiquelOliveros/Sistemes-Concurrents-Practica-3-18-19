63a491ccabf938978c00f3da63a2ce5716b44e4d
MiquelOliveros <misalmiki@gmail.com>
2018-12-19 15:53:57 -0800

    Revisio dels testos

--- Testing/test.sh
+++ Testing/test.sh
@@ -12,10 +12,10 @@
 #Previ a llençar la comanda s'ha de modificar el servidor per a que al cap de 100 clients tancats es tanqui
 #modificant el loop while true
 FTP_PATH="../.."
-for i in {1..50}
+for i in {1..100}
 do
 	cd example_dir
-	$FTP_PATH/siftp localhost 77778 < commands1.txt &
+	$FTP_PATH/siftp localhost 77777 < commands1.txt &
 	cd ..
 	pids[${i}]=$! 
 done
